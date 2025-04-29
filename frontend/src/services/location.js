9f00991e7c5734c9604f881e823325a463c6fb37
Peter Wanner <pgw06@proton.me>
2025-04-28 19:04:39 -0700

    Halfway to geolocation

---
+++ frontend/src/services/location.js
@@ -0,0 +1,22 @@
+const getGeolocation = () => {
+    return new Promise((resolve, reject) => {
+        if (!navigator.geolocation) {
+            reject("Geolocation is not supported by your browser.");
+        }
+    
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                resolve({
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                });
+            },
+            (error) => {
+                reject("Unable to retrieve your location.");
+            }
+        );
+    });
+};
+  
+export default getGeolocation;
+  
